2ece866effaf6b89ac5bc98492eef974fb9daf33
Chris Erlendson <ckesurf@gmail.com>
2023-02-09 14:50:42 -0500

    update user_serializer

--- phase-4/05-authentication/app/serializers/user_serializer.rb
+++ phase-4/05-authentication/app/serializers/user_serializer.rb
@@ -1,9 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email 
-
-  has_many :sold_items
-  has_many :purchased_items
-
-  has_many :sold_categories 
-  has_many :purchased_categories
 end
