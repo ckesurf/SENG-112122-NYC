5ffa5a35b881c28807870657a7052df3303ff934
Chris Erlendson <ckesurf@gmail.com>
2023-02-09 14:44:04 -0500

    remove extraneous routes

--- phase-4/05-authentication/config/routes.rb
+++ phase-4/05-authentication/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :items
   resources :users, only: [:create, :destroy, :index]
-  resources :categories, only: [:index, :show]
-  patch '/items/:id/sold', to: "items#sold"
-  get '/purchased_items', to: 'items#purchased_items'
 end
 
